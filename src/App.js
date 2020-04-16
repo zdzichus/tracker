08f541f52a2814bf080579eb7f1f34e16d4bbd9c
zdzic <zdzic@LAPTOP-HBGRI09D>
2020-04-17 00:04:25 +0200

    comit4

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //import "proper.js/dist/umd/popper.min.js";
 //import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CreateUser from "./components/create-user.component";
+import CreateTimeSheet from "./components/create-timesheet.component";
+import EditTimeSheet from "./components/edit-timesheet.component";
+import TimeSheetList from "./components/timesheet-list.component";
 import EditUser from "./components/edit-user.component";
 import UserList from "./components/user-list.component";
 
@@ -18,34 +21,20 @@ import UserList from "./components/user-list.component";
 
 function App() {
     return (<Router>
-   
-
         <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
-
             <div class="container">
-             
                 <a class="navbar-brand" href="/user-list">Project Tracking</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-				data-target="#navbarColor01" aria-controls="navbarColor01"
-				aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			    </button>
-              
-             <div class="collapse navbar-collapse" id="navbarColor01"></div>
-
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active"><a class="nav-link" href="/user-list">Dashboard
-							<span class="sr-only">(current)</span>
-                    </a></li>
-                    <li class="nav-item"><a class="nav-link" href="/user-list">User List</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="/create-user">Create User</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="/user-list">About</a></li>
-                </ul>
-           
-        
-
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                        <div class="collapse navbar-collapse" id="navbarColor01"></div>
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active"><a class="nav-link" href="/user-list">Dashboard<span class="sr-only">(current)</span></a></li>
+                                <li class="nav-item"><a class="nav-link" href="/user-list">User List</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/create-user">Create User</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/timesheet-list">TimeSheets</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/create-timesheet">Create TimeSheet</a></li>
+                            </ul>
             </div>
         </div><br></br><br></br><br></br>
 
@@ -58,6 +47,9 @@ function App() {
                             <Route path="/create-user" component={CreateUser} />
                             <Route path="/edit-user/:id" component={EditUser} />
                             <Route path="/user-list" component={UserList} />
+                            <Route path="/create-timesheet" component={CreateTimeSheet} />
+                            <Route path="/edit-timesheet" component={EditTimeSheet} />
+                            <Route path="/timesheet-list" component={TimeSheetList} />
                         </Switch>
                     </div>
                 </Col>
---
+++ src/components/edit-timesheet.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateTimeSheet extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Create TimeSheet Component!</p>
+      </div>
+    );
+  }
+}
