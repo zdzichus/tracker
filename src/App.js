0d48e128047418ba980957c3a6353c769fb452f8
zdzic <zdzic@LAPTOP-HBGRI09D>
2020-04-16 21:07:33 +0200

    COMIT6

--- src/App.js
+++ src/App.js
@@ -1,67 +1,70 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/bootstrap.min.css";
+//import "proper.js/dist/umd/popper.min.js";
+//import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import CreateUser from "./components/create-user.component";
 import EditUser from "./components/edit-user.component";
 import UserList from "./components/user-list.component";
 
+
+
 function App() {
     return (<Router>
-        <div className="container">
-            <header >
-                <title>Application</title>
+   
 
-                <Navbar bg="dark" variant="dark">
-                    <Container>
+        <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
 
-                        <Navbar.Brand>
-                            <Link to={"/user-list"} className="nav-link">
-                                Project Tarcking
-                            </Link>
-                        </Navbar.Brand>
+            <div class="container">
+             
+                <a class="navbar-brand" href="/user-list">Project Tracking</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+				data-target="#navbarColor01" aria-controls="navbarColor01"
+				aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			    </button>
+              
+             <div class="collapse navbar-collapse" id="navbarColor01"></div>
 
-                        <Nav className="justify-content-end">
-                            <Nav>
-                                <Link to={"/create-user"} className="nav-link">
-                                    Create User
-                                </Link>
-                            </Nav>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active"><a class="nav-link" href="/user-list">Dashboard
+							<span class="sr-only">(current)</span>
+                    </a></li>
+                    <li class="nav-item"><a class="nav-link" href="/user-list">User List</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="/create-user">Create User</a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="/user-list">About</a></li>
+                </ul>
+           
+        
+
+            </div>
+        </div><br></br><br></br><br></br>
+
+        <Container>
+            <Row>
+                <Col md={12}>
+                    <div className="wrapper">
+                        <Switch>
+                            <Route exact path='/' component={CreateUser} />
+                            <Route path="/create-user" component={CreateUser} />
+                            <Route path="/edit-user/:id" component={EditUser} />
+                            <Route path="/user-list" component={UserList} />
+                        </Switch>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
 
 
-                            <Nav>
-                                <Link to={"/user-list"} className="nav-link">
-                                    Users List
-                                </Link>
-                            </Nav>
-                        </Nav>
-
-                    </Container>
-                </Navbar>
-            </header>
-
-            <Container>
-                <Row>
-                    <Col md={12}>
-                        <div className="wrapper">
-                            <Switch>
-                                <Route exact path='/' component={CreateUser} />
-                                <Route path="/create-user" component={CreateUser} />
-                                <Route path="/edit-user/:id" component={EditUser} />
-                                <Route path="/user-list" component={UserList} />
-                            </Switch>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
     </Router>);
 }
 
